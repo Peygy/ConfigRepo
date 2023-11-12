31363959a93051b16489a3edad8be2a12a468073
Leonid Isakov <isakov.29072004@gmail.com>
2023-11-12 19:41:54 +0300

    Changes

--- 5.py
+++ 5.py
@@ -1,60 +1,78 @@
 from graphviz import Digraph
 import zlib
 import os.path as path
-from os import listdir
+from os import scandir
 
-#D: | cd D:\Programming\MIREA\Конфиг\Практика5\ConfigRepo | dot -Tpng input.dot -o output.png
-
-class GitVisual:
-	def __init__(self, repo_path: str):
-		self.repoPath = repo_path
-		self.repoName = repo_path.split('\\')[-1]
+class GitHistoryVisualizer:
+	def __init__(self, repoPath: str):
 		self.dot = Digraph()
+		self.repoPath = repoPath
+		self.repoName = repoPath.split('\\')[-1]
 
-	def GetBranches(self) -> list:
-		return listdir(path.join(self.repoPath, ".git", "refs", "heads"))
 
-	def GetHeadPath(self) -> str:
-		with open(path.join(self.repoPath, ".git", "HEAD")) as f:
-			return f.readline().strip().split(" ")[1]
+	def ReadTree(self, commitHash):
+		treePath = path.join(self.repoPath, ".git\\objects", commitHash[:2], commitHash[2:])
+		with open(treePath, 'rb') as file:
+			decompressedData = zlib.decompress(file.read())
+			data = decompressedData.split(b'\x00')
+			div = data[1].split()[0]
 
-	def ReadTree(self, hash: str) -> list:
-		with open(path.join(self.repoPath, ".git", "objects", hash[:2], hash[2:]), 'rb') as f:
-			raw_data = zlib.decompress(f.read()).split(b'\x00')
-			delim = raw_data[1].split()[0]
-			return [i.split()[-1].decode() for i in raw_data if delim in i]
+			result = []
+			for i in data:
+				if div in i:
+					result.append(i.split()[-1].decode())
 
-	def ReadCommit(self, hash: str):
-		with open(path.join(self.repoPath, ".git", "objects", hash[:2], hash[2:]), 'rb') as f:
-			raw_data = zlib.decompress(f.read()).split(b'\x00')
-			commit_message = raw_data[1].split(b'\n\n')[1].decode()
+			return result
+
+
+	def ReadCommit(self, commitHash):
+		commitPath = path.join(self.repoPath, ".git\\objects", commitHash[:2], commitHash[2:])
+		with open(commitPath, 'rb') as file:
+			data = zlib.decompress(file.read()).split(b'\x00')
+			sections = data[1].split(b'\n')
+			commitData = data[1].split(b'\n\n')[1].decode()
+
 			files = ""
-			parent_message = None
-			sections = raw_data[1].split(b'\n')
+			headData = None
+
 			for section in sections:
 				if b'tree' in section:
 					files = '\n'.join(self.ReadTree(section.split()[1].decode()))
 				if b'parent' in section:
-					parent_message = self.ReadCommit(section.split()[1].decode())
-			child_message = commit_message + "\n" + files
-			if parent_message is not None:
-				self.AddBlock(parent_message, child_message)
-			return child_message
+					headData = self.ReadCommit(section.split()[1].decode())
 
-	def AddBlock(self, parent_message: str, child_message: str):
-		self.dot.edge(parent_message, child_message)
+			subData = f"{commitData}\n{files}"
+			if headData is not None:
+				self.AddBlock(headData, subData)
+
+			return subData
+
+
+	def AddBlock(self, headData, subData):
+		self.dot.edge(headData, subData)
+
+
+	def GetBranches(self):
+		branchesDir = path.join(self.repoPath, ".git\\refs\\heads")
+		branches = []
+
+		for entry in scandir(branchesDir):
+			branches.append(entry.name)
+
+		return branches
 
 	def BuildDotGraph(self):
 		for branch in self.GetBranches():
-			with open(path.join(self.repoPath, ".git", "refs", "heads", branch)) as f:
+			branchPath = path.join(self.repoPath, ".git\\refs\\heads", branch)
+			with open(branchPath) as file:
 				self.dot.format = 'png'
-				self.ReadCommit(f.readline().strip())
+				self.ReadCommit(file.readline().strip())
 				self.dot.render('input')
 				self.dot.format = 'dot'
 				self.dot.render('input')
 
 
 if __name__ == "__main__":
-	repo_path = "D:\Programming\MIREA\Конфиг\Практика5\ConfigRepo"
-	git = GitVisual(repo_path)
-	git.BuildDotGraph()
+	repoPath = "D:\Programming\MIREA\Конфиг\Практика5\ConfigRepo"
+	visualizer = GitHistoryVisualizer(repoPath)
+	visualizer.BuildDotGraph()
